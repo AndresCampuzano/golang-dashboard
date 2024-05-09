21c97ecb964c227556ca805ee168b8a639e3f821
AndresCampuzano <campuxano88@gmail.com>
2024-05-09 15:11:54 -0500

    wip earnings query, add cities and depts to json response

--- earnings.storage.go
+++ earnings.storage.go
@@ -20,117 +20,148 @@ func (s *PostgresStore) GetEarnings() ([]*Earnings, error) {
 		GROUP BY
 			month, e.currency
 	),
-	all_expenses_in_month AS (
-		SELECT DISTINCT
-			e.id,
-			e.name,
-			e.price,
-			e.type,
-			e.description,
-			e.currency,
-			e.created_at,
-			e.updated_at
-		FROM
-			expenses e
-	),
-	monthly_income AS (
-		SELECT
-			DATE_TRUNC('month', pv.created_at) AS month,
-			SUM(pv.price) AS total_income
-		FROM
-			product_variations pv
-		GROUP BY
-			month
-	),
-	cop_expenses AS (
-		SELECT
-			DATE_TRUNC('month', e.created_at) AS month,
-			SUM(CASE WHEN e.currency = 'COP' THEN e.price ELSE 0 END) AS total_cop_expense
-		FROM
-			expenses e
-		GROUP BY
-			month
-	),
-	sales_count AS (
-		SELECT
-			DATE_TRUNC('month', s.created_at) AS month,
-			COUNT(*) AS total_sales_in_month
-		FROM
-			sales s
-		GROUP BY
-			DATE_TRUNC('month', s.created_at)
-	),
-	total_product_variations AS (
-	SELECT
-		DATE_TRUNC('month', pv.created_at) AS month,
-		COUNT(*) AS total_variations
-	FROM
-		product_variations pv
-	GROUP BY
-		DATE_TRUNC('month', pv.created_at)
-     ),
+		 all_expenses_in_month AS (
+			 SELECT DISTINCT
+				 e.id,
+				 e.name,
+				 e.price,
+				 e.type,
+				 e.description,
+				 e.currency,
+				 e.created_at,
+				 e.updated_at
+			 FROM
+				 expenses e
+		 ),
+		 monthly_income AS (
+			 SELECT
+				 DATE_TRUNC('month', pv.created_at) AS month,
+				 SUM(pv.price) AS total_income
+			 FROM
+				 product_variations pv
+			 GROUP BY
+				 month
+		 ),
+		 cop_expenses AS (
+			 SELECT
+				 DATE_TRUNC('month', e.created_at) AS month,
+				 SUM(CASE WHEN e.currency = 'COP' THEN e.price ELSE 0 END) AS total_cop_expense
+			 FROM
+				 expenses e
+			 GROUP BY
+				 month
+		 ),
+		 sales_count AS (
+			 SELECT
+				 DATE_TRUNC('month', s.created_at) AS month,
+				 COUNT(*) AS total_sales_in_month
+			 FROM
+				 sales s
+			 GROUP BY
+				 DATE_TRUNC('month', s.created_at)
+		 ),
+		 total_product_variations AS (
+			 SELECT
+				 DATE_TRUNC('month', pv.created_at) AS month,
+				 COUNT(*) AS total_variations
+			 FROM
+				 product_variations pv
+			 GROUP BY
+				 DATE_TRUNC('month', pv.created_at)
+		 ),
+		 total_sales_by_city AS (
+			 SELECT
+				 DATE_TRUNC('month', s.created_at) AS month,
+				 s.customer_city AS city,
+				 COUNT(*) AS sales
+			 FROM
+				 sales s
+			 GROUP BY
+				 DATE_TRUNC('month', s.created_at), s.customer_city
+		 ),
+		 total_sales_by_department AS (
+			 SELECT
+				 DATE_TRUNC('month', s.created_at) AS month,
+				 s.customer_department AS department,
+				 COUNT(*) AS sales
+			 FROM
+				 sales s
+			 GROUP BY
+				 DATE_TRUNC('month', s.created_at), s.customer_department
+		 ),
 	-- Generate a distinct list of months from all sources
-	distinct_months AS (
-		SELECT DISTINCT month FROM (
-			SELECT month FROM monthly_expenses
-			UNION
-			SELECT month FROM monthly_income
-			UNION
-			SELECT month FROM cop_expenses
-			UNION
-			SELECT month FROM sales_count
-		) AS all_months
-	)
+		 distinct_months AS (
+			 SELECT DISTINCT month FROM (
+											SELECT month FROM monthly_expenses
+											UNION
+											SELECT month FROM monthly_income
+											UNION
+											SELECT month FROM cop_expenses
+											UNION
+											SELECT month FROM sales_count
+										) AS all_months
+		 )
 	SELECT
 		dm.month AS sort_by_month,
 		COALESCE(
-			(
-				SELECT JSON_AGG(jsonb_build_object('currency', me.currency, 'value', me.total_expense))
-				FROM (
-					SELECT DISTINCT ON (currency) currency, total_expense
-					FROM monthly_expenses
-					WHERE month = dm.month
-					ORDER BY currency
-				) AS me
-			),
-			'[]'
+				(
+					SELECT JSON_AGG(jsonb_build_object('currency', me.currency, 'value', me.total_expense))
+					FROM (
+							 SELECT DISTINCT ON (currency) currency, total_expense
+							 FROM monthly_expenses
+							 WHERE month = dm.month
+							 ORDER BY currency
+						 ) AS me
+				),
+				'[]'
 		) AS expenses_summary,
 		COALESCE(
-			(
-				SELECT JSON_AGG(jsonb_build_object(
-					'id', aem.id,
-					'name', aem.name,
-					'price', aem.price,
-					'type', aem.type,
-					'description', aem.description,
-					'currency', aem.currency,
-					'created_at', aem.created_at,
-					'updated_at', aem.updated_at
-				))
-				FROM all_expenses_in_month aem
-				WHERE DATE_TRUNC('month', aem.created_at) = dm.month
-			),
-			'[]'
+				(
+					SELECT JSON_AGG(jsonb_build_object(
+							'id', aem.id,
+							'name', aem.name,
+							'price', aem.price,
+							'type', aem.type,
+							'description', aem.description,
+							'currency', aem.currency,
+							'created_at', aem.created_at,
+							'updated_at', aem.updated_at
+									))
+					FROM all_expenses_in_month aem
+					WHERE DATE_TRUNC('month', aem.created_at) = dm.month
+				),
+				'[]'
 		) AS all_expenses_in_month,
 		COALESCE(mi.total_income, 0) AS income,
 		COALESCE(ce.total_cop_expense, 0) AS cop_expense,
 		CASE WHEN COALESCE(mi.total_income, 0) - COALESCE(ce.total_cop_expense, 0) < 0 THEN 0
-		 ELSE COALESCE(mi.total_income, 0) - COALESCE(ce.total_cop_expense, 0)
-		END AS earnings,
+			 ELSE COALESCE(mi.total_income, 0) - COALESCE(ce.total_cop_expense, 0)
+			END AS earnings,
 		COALESCE(sc.total_sales_in_month, 0) AS total_sales_in_month,
-		COALESCE(tpv.total_variations, 0) AS total_product_variations_in_month
+		COALESCE(tpv.total_variations, 0) AS total_product_variations_in_month,
+		(
+			SELECT JSON_AGG(jsonb_build_object('name', city, 'sales', sales))
+			FROM total_sales_by_city
+			WHERE DATE_TRUNC('month', total_sales_by_city.month) = dm.month
+		) AS cities,
+		(
+			SELECT JSON_AGG(jsonb_build_object('name', department, 'sales', sales))
+			FROM total_sales_by_department
+			WHERE DATE_TRUNC('month', total_sales_by_department.month) = dm.month
+		) AS departments
 	FROM
 		distinct_months dm
-	LEFT JOIN
+			LEFT JOIN
 		monthly_income mi ON dm.month = mi.month
-	LEFT JOIN
+			LEFT JOIN
 		cop_expenses ce ON dm.month = ce.month
-	LEFT JOIN
+			LEFT JOIN
 		sales_count sc ON dm.month = sc.month
-	LEFT JOIN
+			LEFT JOIN
 		total_product_variations tpv ON dm.month = tpv.month
 	ORDER BY
 		dm.month;
+
 
 	`)
 	if err != nil {
@@ -161,6 +192,8 @@ func scanIntoEarnings(rows *sql.Rows) (*Earnings, error) {
 	earning := new(Earnings)
 	var expensesSummaryJSON []byte
 	var allExpensesInMonthJSON []byte
+	var citiesJSON []byte
+	var departmentsJSON []byte
 	err := rows.Scan(
 		&earning.SortByMonth,
 		&expensesSummaryJSON,
@@ -170,6 +203,8 @@ func scanIntoEarnings(rows *sql.Rows) (*Earnings, error) {
 		&earning.Earnings,
 		&earning.TotalSalesInMonth,
 		&earning.TotalProductVariationsInMonth,
+		&citiesJSON,
+		&departmentsJSON,
 	)
 	if err != nil {
 		return nil, err
@@ -183,6 +218,16 @@ func scanIntoEarnings(rows *sql.Rows) (*Earnings, error) {
 	err = json.Unmarshal(allExpensesInMonthJSON, &earning.AllExpensesInMonth)
 	if err != nil {
 		return nil, fmt.Errorf("error unmarshaling allExpensesInMonthJSON JSON: %v", err)
+	}
+
+	err = json.Unmarshal(citiesJSON, &earning.Cities)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling citiesJSON JSON: %v", err)
+	}
+
+	err = json.Unmarshal(departmentsJSON, &earning.Departments)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling departmentsJSON JSON: %v", err)
 	}
 
 	return earning, nil
--- earnings.types.go
+++ earnings.types.go
@@ -7,6 +7,16 @@ type ExpensesSummary struct {
 	Value    float64 `json:"value"`
 }
 
+type CitiesSummary struct {
+	Name  string `json:"name"`
+	Sales int    `json:"sales"`
+}
+
+type DepartmentsSummary struct {
+	Name  string `json:"name"`
+	Sales int    `json:"sales"`
+}
+
 type Earnings struct {
 	SortByMonth        time.Time         `json:"sort_by_month"`
 	ExpensesSummary    []ExpensesSummary `json:"expenses_summary"`
@@ -20,9 +30,11 @@ type Earnings struct {
 		CreatedAt   string  `json:"created_at"` // FIXME: fix this
 		UpdatedAt   string  `json:"updated_at"` // FIXME: fix this
 	} `json:"all_expenses_in_month"`
-	Income                        float64 `json:"income"`
-	CopExpense                    float64 `json:"cop_expense"`
-	Earnings                      float64 `json:"earnings"`
-	TotalSalesInMonth             int     `json:"total_sales_in_month"`
-	TotalProductVariationsInMonth int     `json:"total_product_variations_in_month"`
+	Income                        float64              `json:"income"`
+	CopExpense                    float64              `json:"cop_expense"`
+	Earnings                      float64              `json:"earnings"`
+	TotalSalesInMonth             int                  `json:"total_sales_in_month"`
+	TotalProductVariationsInMonth int                  `json:"total_product_variations_in_month"`
+	Cities                        []CitiesSummary      `json:"cities"`
+	Departments                   []DepartmentsSummary `json:"departments"`
 }
